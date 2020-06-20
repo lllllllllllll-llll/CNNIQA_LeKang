e8f213264e5cfd74382a6a081f9b5aa01d4dd4b4
normalman <64141791+lllllllllllll-llll@users.noreply.github.com>
2020-06-20 16:31:31 +0800

    Update network.py

--- network.py
+++ network.py
@@ -15,20 +15,14 @@ class CNN(nn.Module):
 
     def forward(self, input):
         x = input.view(-1, input.size(-3), input.size(-2), input.size(-1))
-        # print('x', x.size())#[128, 1, 32, 32]
 
         x = self.conv(x)
-        # print('x', x.size())#[128, 50, 26, 26]
 
         x1 = F.max_pool2d(x, (x.size(-2), x.size(-1)))
-        # print('x1', x1.size())#[128, 50, 1, 1]
         x2 = -F.max_pool2d(-x, (x.size(-2), x.size(-1)))
-        # print('x2', x2.size())#[128, 50, 1, 1]
 
         h = torch.cat((x1, x2), 1)
-        # print('h', h.size())#[128, 100, 1, 1]
         h = h.squeeze(3).squeeze(2)
-        # print('h', h.size())#[128, 100]
 
         h = F.relu(self.fc1(h))
         h = F.dropout(h)
